bf0ea505cf75f740e69f9ba487631395301e183e
Thiago V <thiagovc1500@gmail.com>
2021-10-31 21:52:16 -0300

    Refactored Category model as db entity

--- src/modules/Cars/entities/Category.ts
+++ src/modules/Cars/entities/Category.ts
@@ -1,3 +1,4 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
 /**
@@ -5,10 +6,18 @@ import { v4 as uuidv4 } from "uuid";
  * de dados utilizadas no projeto, e importar essas classes nas rotas
  * designadas.
  */
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   createdAt: Date;
 
   constructor() {
