d50629f68398db54db464e363f89227fc64eff7f
Thiago V <thiagovc1500@gmail.com>
2021-11-25 17:07:31 -0300

    Integration tests

---
+++ src/modules/Cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,70 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { app } from "../../../../app";
+import createConnection from "../../../../shared/infra/typeorm";
+
+let connection: Connection;
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash("admin", 8);
+
+    await connection.query(`
+      INSERT INTO users(id, name, email, password, "admin", "createdAt", "driverLicense")
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'ASD-4568')
+    `);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category supertest description",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should not be able to create a new category with an existing name", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Desc for category supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
