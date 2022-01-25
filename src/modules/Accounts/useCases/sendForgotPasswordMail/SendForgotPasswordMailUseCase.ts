6b68d2976bf00e08319d054f25f67c487acb68b2
Thiago Viana <thiagovc1500@gmail.com>
2022-01-25 11:18:12 -0300

    Implemented email provider (Ethereal)

--- src/modules/Accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/Accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,7 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidv4 } from "uuid";
-import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
 
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "../../../../shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "../../../../shared/errors/AppError";
 import { IUsersRepository } from "../../repositories/Users/IUsersRepository";
 import { IUsersTokensRepository } from "../../repositories/Users/IUsersTokensRepository";
@@ -14,7 +15,9 @@ class SendForgotPasswordMailUseCase {
     @inject("UsersTokensRepository")
     private usersTokensRepository: IUsersTokensRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(email: string) {
@@ -33,6 +36,12 @@ class SendForgotPasswordMailUseCase {
       userId: user.id,
       expiresDate,
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de Senha",
+      `O link para o reset é ${token}`
+    );
   }
 }
 
